9fec7a747ad5cb1096c4f3927530f3ad4d429580
Dima <vorotnikov7009@mail.ru>
2023-03-28 23:00:17 +0400

    ДЗ 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item1():
+    return Item('phone_test', 10000, 3)
+
+
+def test_item_init(item1):
+    assert item1.name == 'phone_test'
+    assert item1.price == 10000
+    assert item1.quantity == 3
+
+
+def test_item_calculate_total_price(item1):
+    assert item1.price == 10000
+
+def test_item_apply_discount(item1):
+    item1.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000
