101c33b3b983cb4943155e49d44e792a78e07bf2
Dima <vorotnikov7009@mail.ru>
2023-04-06 23:51:13 +0400

    ДЗ 2 дополнил

--- src/item.py
+++ src/item.py
@@ -33,7 +33,7 @@ class Item:
     @name.setter
     def name(self, new_name: str) -> None:
         if len(new_name) > 10:
-            print('Длина наименования товара превышает 10 символов')
+            raise Exception("Длина наименования товара превышает 10 символов.")
         else:
             self.__name = new_name
 
